6d07c03330c3bb15352c200bc648bc241ad26ec4
tennyhoang <dotruongphat9@gmail.com>
2026-01-17 10:20:00 +0700

    Implement GlassCategoryModel for Glasses Online System

---
+++ GlassCategoryModel.java
@@ -0,0 +1,8 @@
+public class GlassCategoryModel {
+    // Logic for Glasses Online Shop - Day 17
+    public void execute() {
+        System.out.println("Processing GlassCategoryModel...");
+        // Business logic for online glass store
+        // Adding more lines to increase ++ stats
+    }
+}
