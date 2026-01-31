15f78804ec5ea9ef1925e1555432d7f289d70930
DoTruongPhat <dotruongphat9@gmail.com>
2026-01-31 15:21:35 +0700

    Delete AuthInterceptor.java

--- AuthInterceptor.java
+++
@@ -1,8 +0,0 @@
-public class AuthInterceptor {
-    // Logic for Glasses Online Shop - Day 16
-    public void execute() {
-        System.out.println("Processing AuthInterceptor...");
-        // Business logic for online glass store
-        // Adding more lines to increase ++ stats
-    }
-}
