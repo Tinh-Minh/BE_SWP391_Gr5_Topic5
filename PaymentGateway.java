8c66b806ece9fe7e094b612b29d1e0793c34b0ee
DoTruongPhat <dotruongphat9@gmail.com>
2026-02-01 20:47:05 +0700

    Delete PaymentGateway.java

--- PaymentGateway.java
+++
@@ -1,8 +0,0 @@
-public class PaymentGateway {
-    // Logic for Glasses Online Shop - Day 18
-    public void execute() {
-        System.out.println("Processing PaymentGateway...");
-        // Business logic for online glass store
-        // Adding more lines to increase ++ stats
-    }
-}
