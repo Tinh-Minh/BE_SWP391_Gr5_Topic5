aebe402863e92b1be5cd4dce65c54644bd0acde6
DoTruongPhat <dotruongphat9@gmail.com>
2026-02-01 20:44:15 +0700

    Delete GlassCategoryModel.java

--- GlassCategoryModel.java
+++
@@ -1,8 +0,0 @@
-public class GlassCategoryModel {
-    // Logic for Glasses Online Shop - Day 17
-    public void execute() {
-        System.out.println("Processing GlassCategoryModel...");
-        // Business logic for online glass store
-        // Adding more lines to increase ++ stats
-    }
-}
