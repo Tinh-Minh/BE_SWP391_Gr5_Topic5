51dfe8fb9514b20209f5585671decc163e1aad2e
DoTruongPhat <dotruongphat9@gmail.com>
2026-02-01 20:44:07 +0700

    Delete DiscountLogic.java

--- DiscountLogic.java
+++
@@ -1,8 +0,0 @@
-public class DiscountLogic {
-    // Logic for Glasses Online Shop - Day 19
-    public void execute() {
-        System.out.println("Processing DiscountLogic...");
-        // Business logic for online glass store
-        // Adding more lines to increase ++ stats
-    }
-}
