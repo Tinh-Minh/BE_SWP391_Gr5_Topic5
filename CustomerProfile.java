4e7dabca24ba531e19789d892ef4d3960704e34a
DoTruongPhat <dotruongphat9@gmail.com>
2026-02-01 20:43:52 +0700

    Delete CustomerProfile.java

--- CustomerProfile.java
+++
@@ -1,8 +0,0 @@
-public class CustomerProfile {
-    // Logic for Glasses Online Shop - Day 20
-    public void execute() {
-        System.out.println("Processing CustomerProfile...");
-        // Business logic for online glass store
-        // Adding more lines to increase ++ stats
-    }
-}
