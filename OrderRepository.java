c708f9eab2be923fde4b6f78603ad07962770cdf
DoTruongPhat <dotruongphat9@gmail.com>
2026-02-01 20:46:56 +0700

    Delete OrderRepository.java

--- OrderRepository.java
+++
@@ -1,8 +0,0 @@
-public class OrderRepository {
-    // Logic for Glasses Online Shop - Day 15
-    public void execute() {
-        System.out.println("Processing OrderRepository...");
-        // Business logic for online glass store
-        // Adding more lines to increase ++ stats
-    }
-}
