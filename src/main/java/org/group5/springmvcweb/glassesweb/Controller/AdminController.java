137c8763f6d87eb85bfada8fb2d6b5d618db5f06
Đoàn Văn Minh <minh1906204@gmail.com>
2026-02-28 23:48:30 +0700

    UPDATE Profile

--- src/main/java/org/group5/springmvcweb/glassesweb/Controller/AdminController.java
+++ src/main/java/org/group5/springmvcweb/glassesweb/Controller/AdminController.java
@@ -1,8 +1,10 @@
 package org.group5.springmvcweb.glassesweb.Controller;
 
 import jakarta.validation.Valid;
+import org.group5.springmvcweb.glassesweb.DTO.UpdateProfileRequest;
 import org.group5.springmvcweb.glassesweb.DTO.UpdateRoleRequest;
 import org.group5.springmvcweb.glassesweb.Service.AuthService;
+import org.group5.springmvcweb.glassesweb.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ public class AdminController {
 
     @Autowired
     private AuthService authService;
+    private CustomerService customerService;
 
     @PutMapping("/accounts/{id}/role")
     public ResponseEntity<Map<String, String>> updateRole(
@@ -22,5 +25,14 @@ public class AdminController {
             @Valid @RequestBody UpdateRoleRequest request) {
         authService.updateRole(id, request.getRole());
         return ResponseEntity.ok(Map.of("message", "Role update successful!"));
+    }
+    // Admin update profile cho bất kỳ ai
+    @PutMapping("/customers/{id}/profile")
+    public ResponseEntity<Map<String, String>> adminUpdateProfile(
+            @PathVariable Integer id,
+            @RequestBody UpdateProfileRequest request) {
+
+        customerService.adminUpdateProfile(id, request);
+        return ResponseEntity.ok(Map.of("message", "Cập nhật thông tin thành công!"));
     }
 }
