dc108812aa076e740f2261aa22edd35030024955
tennyhoang <90254229+tennyhoang@users.noreply.github.com>
2026-03-04 13:11:09 +0700

    upload 2 function
     EyeProfile + Design Domain

--- src/main/java/org/group5/springmvcweb/glassesweb/Repository/AccountRepository.java
+++ src/main/java/org/group5/springmvcweb/glassesweb/Repository/AccountRepository.java
@@ -1,8 +1,11 @@
 package org.group5.springmvcweb.glassesweb.Repository;
 
-import org.group5.springmvcweb.glassesweb.Entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.group5.springmvcweb.glassesweb.Entity.Account;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
-    Account findByUsername(String username);
+    Optional<Account> findByUsername(String username);
+    boolean existsByUsername(String username);
+
 }
