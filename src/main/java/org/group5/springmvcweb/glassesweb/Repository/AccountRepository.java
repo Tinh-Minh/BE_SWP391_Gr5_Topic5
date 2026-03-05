213cd2a9dee5297775739669c44f821d6c98ed7e
DoTruongPhat <dotruongphat9@gmail.com>
2026-03-05 12:35:59 +0700

    Delete src directory

--- src/main/java/org/group5/springmvcweb/glassesweb/Repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package org.group5.springmvcweb.glassesweb.Repository;
-
-import org.group5.springmvcweb.glassesweb.Entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    Account findByUsername(String username);
-}
