f0c1868f637fc79787344e661f0c802d4fadb4f4
DoTruongPhat <dotruongphat9@gmail.com>
2026-03-05 12:33:22 +0700

    Delete GlassesWeb - Copy directory

--- GlassesWeb - Copy/src/main/java/org/group5/springmvcweb/glassesweb/Repository/FrameRepository.java
+++
@@ -1,7 +0,0 @@
-package org.group5.springmvcweb.glassesweb.Repository;
-
-import org.group5.springmvcweb.glassesweb.Entity.Frame;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FrameRepository extends JpaRepository<Frame,Integer> {
-}
